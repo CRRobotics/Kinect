47fab94b8ff4cbf25947fe2dad203ef3613a9e92
Backgammon <levibackgammon@gmail.com>
2012-03-25 23:10:54 -0400

    Changed FilterBrightSub to take a const reference; corrected typos.

--- hulltest/FilterBright.h
+++ hulltest/FilterBright.h
@@ -2,7 +2,7 @@
 #define PIXELCOUNT_H
 
 void FilterBrightRects(vector<PolyVertices> &list, IplImage *img);
-bool FilterBrightSub(PolyVertices poly, IplImage *img);
+bool FilterBrightSub(const PolyVertices &poly, IplImage *img);
 void GetBounds(int x0, int x1, int y0, int y1, vector<int> &bounds);
 
 #endif
